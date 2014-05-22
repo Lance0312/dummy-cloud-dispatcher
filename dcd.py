a978627b113a82d131dc7a49fd2de0bf251ad239
Lance Chen <cyen0312@gmail.com>
2014-05-23 03:53:04 +0800

    Add database support
    Signed-off-by: Lance Chen <cyen0312@gmail.com>

--- dcd.py
+++ dcd.py
@@ -5,9 +5,13 @@ from novaclient.client import Client
 from novaclient import exceptions
 from flask import Flask, render_template, request
 from wtforms import Form, validators
-from wtforms import TextField, PasswordField, DateTimeField, TextAreaField
+from wtforms import TextField, PasswordField, TextAreaField
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 app = Flask(__name__)
+app.config.from_envvar('DCD_SETTINGS')
+db = SQLAlchemy(app)
 
 
 class DeployForm(Form):
@@ -16,7 +20,37 @@ class DeployForm(Form):
     project = TextField('Project Name', [validators.InputRequired()])
     endpoint = TextField('Endpoint', [validators.InputRequired()])
     memo = TextAreaField('Memo')
-    timestamp = DateTimeField('Timestamp', default=datetime.now())
+
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    endpoint = db.Column(db.String(4096))
+    username = db.Column(db.String(4096))
+    msg = db.Column(db.Text())
+    memo = db.Column(db.Text())
+    ip = db.Column(db.String(100))
+    timestamp = db.Column(db.DateTime())
+
+    def __init__(self, endpoint, username, msg, memo):
+        self.endpoint = endpoint
+        self.username = username
+        self.msg = msg
+        self.memo = memo
+        self.ip = request.remote_addr
+        self.timestamp = datetime.now()
+
+    def __repr__(self):
+        return '<Record %r, %r>' % (self.ip, self.endpoint)
+
+    @validates('endpoint')
+    def validate_endpoint(self, key, endpoint):
+        assert len(endpoint) <= 4096
+        return endpoint
+
+    @validates('username')
+    def validate_username(self, key, username):
+        assert len(username) <= 4096
+        return username
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -24,6 +58,7 @@ def deploy():
     form = DeployForm(request.form)
     status = ""
     msg = ""
+    errmsg = ""
     if request.method == 'POST' and form.validate():
         try:
             nova = Client(2, form.username.data, form.password.data,
@@ -37,16 +72,27 @@ def deploy():
         except exceptions.ClientException as e:
             status = "error"
             msg = str(e)
+            errmsg = str(e)
         except requests.exceptions.ConnectionError as e:
             status = "error"
             msg = "Fail to connect to %s" % form.endpoint.data
+            errmsg = str(e)
         except IndexError as e:
             status = "error"
             msg = "No valid image or flavor"
+            errmsg = str(e)
         except Exception as e:
             status = "error"
             msg = "Something goes wrong, you might want to contact the admin."
+            errmsg = str(e)
         finally:
+            try:
+                record = Record(form.endpoint.data, form.username.data,
+                                errmsg, form.memo.data)
+                db.session.add(record)
+                db.session.commit()
+            except Exception as e:
+                print str(e)
             return render_template("form.html", form=form,
                                    status=status, msg=msg)
 
