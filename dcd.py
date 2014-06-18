aebd6b2afccc6d01149b1753b3b026044054f9f9
Lance Chen <cyen0312@gmail.com>
2014-06-18 21:11:57 +0800

    Save task results permanently
    Signed-off-by: Lance Chen <cyen0312@gmail.com>

--- dcd.py
+++ dcd.py
@@ -13,7 +13,8 @@ from celery.task import current
 
 
 def make_celery(app):
-    celery = Celery("dcd", broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery("dcd", broker=app.config['CELERY_BROKER_URL'],
+                    backend=app.config['CELERY_RESULT_BACKEND'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
 
